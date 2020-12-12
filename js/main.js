b08726ac8e50a9f116eb8bbf3159bbeaa4ef2b7f
JR Samodal <jrsamodal@hotmail.com>
2020-12-12 04:35:27 -0800

    added letters, mistake counter, image of the hangman, reset button, and bootstrap. Now adding the word to guess.

---
+++ js/main.js
@@ -0,0 +1,87 @@
+const words = [
+    "Avalanche",
+    "Crave",
+    "Jesus",
+    "balaklava",
+    "donut",
+    "glazed",
+    'sour cream',
+    "whip cream",
+    "camera",
+    "babes",
+    "stand",
+    "stuff toy",
+    "guitar",
+    "sound board",
+    "i hate studying",
+    "little duck",
+    "black duck",
+    "move on",
+    "the moon",
+    "chicken nugget",
+    "christmas",
+    "lynn canyon",
+    "grizzly bear",
+    "onomatopoeia",
+    "happy birthday",
+    "supercalifragilisticexpialidocious",
+    "iphone eleven",
+    "chocolate",
+    "strawberry milkshake",
+    "eyebrows",
+    "hopeless romantic",
+    "oahu hawaii",
+    "hannah montana",
+    "correct answer",
+    "kirkland",
+    "facebook",
+    "instagram algorithm",
+    "kylie jenner",
+    "kobe bryant",
+    "mini guitar",
+    "happy feet",
+    "covid nineteen",
+    "mukbang",
+    "marketplace",
+
+
+
+
+]
+
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
+
+const randomWord = ()=> {
+    answer = words[Math.floor(Math.random()* words.length)];
+    alert(answer)   
+}
+
+const generateLetters = ()=> {
+    let buttonsHTML = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter =>
+       `
+        <button
+            class="btn btn-lg btn-primary m-2"
+            id= '`+ letter +`
+            onClick="handleGuess('` + letter + `')"
+        '>
+            ` + letter + `
+
+
+        </button>`
+        ).join('');
+        document.getElementById('keyboard').innerHTML = buttonsHTML    
+}
+
+const guessedWord = () => {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : "_").join(''));
+
+    document.getElementById("wordsSpot").innerHtml
+}
+
+document.getElementById('maxWrong').innerHTML = maxWrong
+randomWord()
+generateLetters();
